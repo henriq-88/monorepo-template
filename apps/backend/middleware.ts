9af0197fe43d9764a5285a98eb38cbfa2451c0c1
Henrik Wassdahl <henriq88@gmail.com>
2023-01-21 01:46:45 +0900

    fix(backend): cors origin

--- apps/backend/middleware.ts
+++ apps/backend/middleware.ts
@@ -6,6 +6,7 @@ const cors = Cors({
     /https?:\/\/localhost:\d{4,5}/,
     /https:\/\/monorepo-template(-\w+)\.vercel\.app/, // e.g. https://monorepo-template-web.vercel.app
     /https:\/\/monorepo-template(-\w+)-henriq-88\.vercel\.app/, // e.g. https://monorepo-template-orebq1szf-henriq-88.vercel.app or https://monorepo-template-web-henriq-88.vercel.app
+    /https:\/\/monorepo-template(-\w+){2}-henriq-88\.vercel\.app/, // e.g. https://monorepo-template-admin-mkikf3i03-henriq-88.vercel.app
     /https:\/\/monorepo-template(-\w+)-git-main-henriq-88\.vercel\.app/, // e.g. https://monorepo-template-web-git-main-henriq-88.vercel.app
   ],
 });
