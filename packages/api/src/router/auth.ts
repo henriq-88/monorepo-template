8a963991fe5eea83e11a158be14f232359a3ef2e
Henrik Wassdahl <henriq88@gmail.com>
2023-03-22 20:14:52 +0900

    fix: auth api

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -1,15 +1,15 @@
 import { protectedProcedure, publicProcedure, createTRPCRouter } from "../trpc";
 
 export const authRouter = createTRPCRouter({
-  // getSession: publicProcedure
-  //   .meta({ openapi: { method: "GET", path: "/auth/getSession" } })
-  //   .query(({ ctx }) => {
-  //     return ctx.session;
-  //   }),
-  // getSecretMessage: protectedProcedure
-  //   .meta({ openapi: { method: "GET", path: "/auth/getSecretMessage" } })
-  //   .query(() => {
-  //     // testing type validation of overridden next-auth Session in @wassdahl/auth package
-  //     return "you can see this secret message!";
-  //   }),
+  getSession: publicProcedure
+    .meta({ openapi: { method: "GET", path: "/auth/getSession" } })
+    .query(({ ctx }) => {
+      return ctx.session;
+    }),
+  getSecretMessage: protectedProcedure
+    .meta({ openapi: { method: "GET", path: "/auth/getSecretMessage" } })
+    .query(() => {
+      // testing type validation of overridden next-auth Session in @wassdahl/auth package
+      return "you can see this secret message!";
+    }),
 });
